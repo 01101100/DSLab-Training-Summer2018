92b98f6e188c23ab7bcb0132466812b6576dced5
Trinh Thien Long <long.dragon96@gmail.com>
2018-07-27 16:25:58 +0700

    all done

--- tf_idf.py
+++ tf_idf.py
@@ -1,10 +1,13 @@
+from collections import defaultdict
 from os import listdir
 from os.path import isfile
 import numpy as np
 from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+import re
 
 def gather_20newgroups_data():
-    path = '../datasets/20news-bydate/'
+    path = 'datasets/20news-bydate/'
     dirs = [path + dir_name + '/'
             for dir_name in listdir(path)
             if not isfile(path + dir_name)]
@@ -14,23 +17,24 @@ def gather_20newgroups_data():
                        for newsgroup in listdir(train_dir)]
     list_newsgroups.sort()
 
-    with open('../datasets/20news-bydate/stop_words.txt') as f:
-        stop_words = f.read().splitlines()
+    # with open('datasets/20news-bydate/stop_words.txt') as f:
+    #     stop_words = f.read().splitlines()
     stemmer = PorterStemmer()
 
     def collect_data_from(parent_dir, newsgroup_list):
+        stop_words = set(stopwords.words('english'))
         data = []
         for group_id, newsgroup in enumerate(newsgroup_list):
             label = group_id
-            dir_path = parent_dir + '/' + newsgroup + '/'
+            dir_path = parent_dir + newsgroup + '/'
             files = [(filename, dir_path + filename)
                      for filename in listdir(dir_path)
                      if isfile(dir_path + filename)]
             files.sort()
 
-            for filename, filepath in files:
-                with open(filepath) as f:
-                    text = f.read().lower()
+            for filename, file_path in files:
+                with open(file_path, 'r', errors='ignore') as opening_file:
+                    text = opening_file.read().lower()
                     words = [stemmer.stem(word)
                              for word in re.split('\W+', text)
                              if word not in stop_words]
@@ -50,13 +54,13 @@ def gather_20newgroups_data():
     )
 
     full_data = train_data + test_data
-    with open('../datasets/20news-bydate/20news-train-processed.txt', 'w') as f:
+    with open('datasets/20news-bydate/20news-train-processed.txt', 'w') as f:
         f.write('\n'.join(train_data))
 
-    with open('../datasets/20news-bydate/20news-test-processed.txt', 'w') as f:
+    with open('datasets/20news-bydate/20news-test-processed.txt', 'w') as f:
         f.write('\n'.join(test_data))
 
-    with open('../datasets/20news-bydate/20news-full-processed.txt', 'w') as f:
+    with open('datasets/20news-bydate/20news-full-processed.txt', 'w') as f:
         f.write('\n'.join(full_data))
 
 
@@ -81,15 +85,15 @@ def generate_vocabulary(data_path):
                   for word, document_freq in
                   zip(doc_count.keys(), doc_count.values())
                   if document_freq > 10 and not word.isdigit()]
-    words_idfs.sort(key=lambda (word, idf): -idf)
+    # words_idfs.sort(key=lambda (word, idf): -idf) #FIXME: tuple unpacking is not supported in python 3
     print('Vocabulary size: {}'.format(len(words_idfs)))
-    with open('../datasets/20news-bydate/words_idfs.txt', 'w') as f:
+    with open('datasets/20news-bydate/words_idfs.txt', 'w') as f:
         f.write('\n'.join([word + '<fff>' + str(idf) for word, idf in words_idfs]))
 
 
 def get_tf_idf(data_path):
-    with open('..datasets/20news-bydate/words_idfs.txt') as f:
-        words_idf = [(line.split('<fff>')[0], float(line.split('<fff>')[1]))
+    with open('datasets/20news-bydate/words_idfs.txt') as f:
+        words_idfs = [(line.split('<fff>')[0], float(line.split('<fff>')[1]))
                      for line in f.read().splitlines()]
 
         word_IDs = dict([(word, index)
@@ -123,4 +127,9 @@ def get_tf_idf(data_path):
                                        for index, tf_idf_value in words_tfidfs]
             spares_rep = ' '.join(words_tfidfs_normalized)
             data_tf_idf.append((label, doc_id, spares_rep))
+    with open('datasets/20news-bydate/words_tf_idfs.txt', 'w') as f:
+        f.write('\n'.join([str(label) + "<fff>" + str(doc_id) + "<fff>" + spares_rep for label, doc_id, spares_rep in data_tf_idf]))
 
+gather_20newgroups_data()
+generate_vocabulary("datasets/20news-bydate/20news-full-processed.txt")
+get_tf_idf("datasets/20news-bydate/20news-full-processed.txt")
