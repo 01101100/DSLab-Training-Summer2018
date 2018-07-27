53a2e3ac0355fdd9cee804a45f0f1849759095f5
Trinh Thien Long <long.dragon96@gmail.com>
2018-07-27 17:56:44 +0700

    refactor

---
+++ Cluster.py
@@ -0,0 +1,10 @@
+class Cluster:
+    def __init__(self):
+        self._centroid = None
+        self._members = []
+
+    def reset_members(self):
+        self._members = []
+
+    def add_member(self, member):
+        self._members.append(member)
---
+++ Member.py
@@ -0,0 +1,5 @@
+class Member:
+    def __init__(self, r_d, label=None, doc_id=None):
+        self._r_d = r_d
+        self._label = label
+        self._doc_id = doc_id
